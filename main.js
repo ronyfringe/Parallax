180aba28e79b63ac57422928a6fbb9cc797866be
zaza <contact@zaza.com>
2020-05-06 22:18:29 -0500

    primer commmit

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	
+	$(window).scroll(function(event) {
+		var barra = $(window).scrollTop();
+		var posicion = barra * 0.05;
+
+		$('body').css({
+			'background-position': '0 -' + posicion + 'px'
+		});
+	});
+
+});
+
